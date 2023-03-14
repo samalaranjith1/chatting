de473d3d97535104e4f96e950d4bcf0aa18688b6
Ranjith Samala <samalaranjith1@gmail.com>
2023-03-15 05:07:07 +0530

    FC

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import firebase from 'firebase/app'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chat privately</h1>
+    </div>
+  );
+}
+
+export default App;
